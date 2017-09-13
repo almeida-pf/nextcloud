5c9ae28d1840a1b517445ab0054908a03748129c
Shadow <pablot.i@hotmail.com>
2017-09-12 21:44:37 -0300

    Tradução Arquivo fail2ban

---
+++ apps/fail2ban.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# Pablo Almeida - 2017
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/almeida-pf/nextcloud/master/Lib.sh)
+
+# Verifica se ha erros no codigo e aborta se algo nao estiver correto
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Verifica se e root
+if ! is_root
+then
+    printf "\n${Red}Desculpe, Voce nao e ROOT.\n${Color_Off}Voce deve digitar: ${Cyan}sudo ${Color_Off}bash %s/fail2ban.sh\n" "$SCRIPTS"
+    sleep 3
+    exit 1
+fi
+
+### Variaveis locais ###
+# Localizacao dos logs Nextcloud
+NCLOG="$(find / -name nextcloud.log)"
+# Tempo para proibir um IP que excedeu as tentativas
+BANTIME_=600000
+# Tempo de cooldown para senhas incorretas
+FINDTIME_=1800
+# Tentativas falhadas antes de proibir um IP
+MAXRETRY_=10
+
+echo "Instalando Fail2ban..."
+
+apt update -q4 & spinner_loading
+check_command apt install fail2ban -y
+check_command update-rc.d fail2ban disable
+
+if [ -z "$NCLOG" ]
+then
+    echo "nextcloud.log not found"
+    echo "Please add your logpath to $NCPATH/config/config.php and restart this script."
+    exit 1
+else
+    chown www-data:www-data "$NCLOG"
+fi
+
+# Set values in config.php
+sudo -u www-data php "$NCPATH/occ" config:system:set loglevel --value=2
+sudo -u www-data php "$NCPATH/occ" config:system:set log_type --value=file
+sudo -u www-data php "$NCPATH/occ" config:system:set logfile  --value="$NCLOG"
+sudo -u www-data php "$NCPATH/occ" config:system:set logtimezone  --value="$(cat /etc/timezone)"
+
+# Create nextcloud.conf file
+cat << NCONF > /etc/fail2ban/filter.d/nextcloud.conf
+[Definition]
+failregex = ^.*Login failed: '.*' \(Remote IP: '<HOST>'.*$
+ignoreregex =
+NCONF
+
+# Create jail.local file
+cat << FCONF > /etc/fail2ban/jail.local
+# The DEFAULT allows a global definition of the options. They can be overridden
+# in each jail afterwards.
+[DEFAULT]
+
+# "ignoreip" can be an IP address, a CIDR mask or a DNS host. Fail2ban will not
+# ban a host which matches an address in this list. Several addresses can be
+# defined using space separator.
+ignoreip = 127.0.0.1/8 192.168.0.0/16 172.16.0.0/12 10.0.0.0/8
+
+# "bantime" is the number of seconds that a host is banned.
+bantime  = $BANTIME_
+
+# A host is banned if it has generated "maxretry" during the last "findtime"
+# seconds.
+findtime = $FINDTIME_
+maxretry = $MAXRETRY_
+
+#
+# ACTIONS
+#
+banaction = iptables-multiport
+protocol = tcp
+chain = INPUT
+action_ = %(banaction)s[name=%(__name__)s, port="%(port)s", protocol="%(protocol)s", chain="%(chain)s"]
+action_mw = %(banaction)s[name=%(__name__)s, port="%(port)s", protocol="%(protocol)s", chain="%(chain)s"]
+action_mwl = %(banaction)s[name=%(__name__)s, port="%(port)s", protocol="%(protocol)s", chain="%(chain)s"]
+action = %(action_)s
+
+#
+# SSH
+#
+
+[ssh]
+
+enabled  = true
+port     = ssh
+filter   = sshd
+logpath  = /var/log/auth.log
+maxretry = $MAXRETRY_
+
+#
+# HTTP servers
+#
+
+[nextcloud]
+
+enabled  = true
+port     = http,https
+filter   = nextcloud
+logpath  = $NCLOG
+maxretry = $MAXRETRY_
+FCONF
+
+# Update settings
+check_command update-rc.d fail2ban defaults
+check_command update-rc.d fail2ban enable
+check_command service fail2ban restart
+
+# The End
+echo
+echo "Fail2ban is now sucessfully installed."
+echo "Please use 'fail2ban-client set nextcloud unbanip <Banned IP>' to unban certain IPs"
+echo "You can also use 'iptables -L -n' to check which IPs that are banned"
+any_key "Press any key to continue..."
+clear
