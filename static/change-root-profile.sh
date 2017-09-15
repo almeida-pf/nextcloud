6d5fb1e6b544bc979b244216642b3eb424300ead
Shadow <pablot.i@hotmail.com>
2017-09-15 14:57:27 -0300

    Tradução arquivo change-root-profile

---
+++ static/change-root-profile.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Pablo Almeida - 2017
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/almeida-pf/nextcloud/master/Lib.sh)
+
+# Verifica se ha erros no codigo e aborta se algo nao estiver correto
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+[ -f /root/.profile ] && rm -f /root/.profile
+
+cat <<ROOT-PROFILE > "$ROOT_PROFILE"
+
+# ~/.profile: executado por shell de login compativeis com Bourne.
+
+if [ "$BASH" ]
+then
+    if [ -f ~/.bashrc ]
+    then
+        . ~/.bashrc
+    fi
+fi
+
+if [ -x /var/scripts/nextcloud-startup-script.sh ]
+then
+    /var/scripts/nextcloud-startup-script.sh
+fi
+
+if [ -x /var/scripts/history.sh ]
+then
+    /var/scripts/history.sh
+fi
+
+mesg n
+
+ROOT-PROFILE
