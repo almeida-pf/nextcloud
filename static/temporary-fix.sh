e7c935166e2b0500f39348d27bd30376538e549c
Shadow <pablot.i@hotmail.com>
2017-09-16 22:54:37 -0300

    Tradução arquivo temporary-fix

---
+++ static/temporary-fix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pablo Almeida - 2017
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL curl -sL https://raw.githubusercontent.com/almeida-pf/nextcloud/master/Lib.sh)
+
+# Verifique se ha erros no codigo e aborta se algo nao estiver correto
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+exit
